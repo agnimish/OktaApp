8520febcf06580229bff84defcacaa99fa020729
Nimish Agarwal <agnimish@iitk.ac.in>
2019-03-17 22:56:10 +0530

    Added ServiceWorker for offline loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById('root'));
+registerServiceWorker();
