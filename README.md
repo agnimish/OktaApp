5a0791212fb1498bcd9981065faa9be488f96bd3
Nimish Agarwal <agnimish@iitk.ac.in>
2019-03-23 11:13:07 +0530

    added required modules

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Octa-App
-An application made for Octa hackathon which serves as a hub to all of your transactions.
+# Okta-App
+An application made for Okta hackathon which serves as a hub to all of your transactions.
 
 ## Steps for running application on phone and web.
 
--- src/config.js
+++ src/config.js
@@ -4,6 +4,11 @@ export const config_web = {
   client_id: '0oacuk1msUGa9iaIQ356'
 }
 
-export const config_mobile = {
-    
-}
+// export const config_mobile = {
+//     issuer: 'https://dev-304959.okta.com/oauth2/default',
+//     client_id: '0oad4f89j3Dl0iaEG356',
+//     scope: 'openid profile',
+//     redirect_uri: __DEV__ ?
+//     '//localhost:19000/+expo-auth-session' :
+//     '{yourOktaScheme}:/+expo-auth-session'
+// }
